7ff3114391ac1f7338f8ff82634a9d7361ef46a2
kotama7 <kotamatakanori2@gmail.com>
2023-02-12 16:36:42 +0900

    表記を変更

--- tkinter_extension_module/get_tk_images.py
+++ tkinter_extension_module/get_tk_images.py
@@ -7,20 +7,20 @@ def get_tk_images(path_list:list,size_list:list) -> list:
         The attribute of path_list must be list of string which is path of image file.
         The attribute of size_list must be list of list likes (width, height).
         Additionally, all members of size_list must be list which have two int elements.
-        The length of path_list and size_list must be same.
+        the length of path_list and size_list must be same.
     """
     if(len(path_list) != len(size_list)):
-        raise Exception("The length of path_list and size_list must be same.")
+        raise Exception("the length of path_list and size_list must be same.")
     image_ls = []
     for path, size in zip(path_list, size_list):
         if type(path) is not str:
-            raise Exception("All members of path_list must be string")
+            raise Exception("all members of path_list must be string")
         if len(size) != 2 or not (type(size[0]) is int and type(size[1]) is int):
-            raise Exception("All members of size_list must be (width, height) and both of that is integer")
+            raise Exception("all members of size_list must be (width, height) and both of that is integer")
         try:
             img = Image.open(path)
         except FileNotFoundError:
-            raise FileNotFoundError(f"FileNotFoundError:\nThere is no file having name : {path}")
+            raise FileNotFoundError(f"There is no file having name : {path}")
         img = img.resize((size[0],size[1]))
         tk_img = ImageTk.PhotoImage(img)
         image_ls.append(tk_img)
